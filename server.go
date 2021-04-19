57c4b66d70ad26fd8d484b9baa339c997acad67f
OMOREAU <moreauolivia49@gmail.com>
2021-04-19 08:21:04 +0200

    importation donnée JSON en JS

--- server.go
+++ server.go
@@ -1,17 +1,84 @@
 package main
 
 import (
+	"fmt"
+	"text/template"
+	"encoding/json"
+	"strconv"
+	"io/ioutil"
 	"net/http"
 	handlers "./src"
 )
 
+const RESTAPIURL string = "https://groupietrackers.herokuapp.com/api/artists"
+// const RESTAPIURL string = "https://groupietrackers.herokuapp.com/api/artistes"
+
+type Artist struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+}
+
+type PageDataArtist struct {
+	Artists []Artist
+}
+
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
+	
 	http.HandleFunc("/", handlers.Accueil)
 	http.HandleFunc("/artistes", handlers.Artistes)
 	http.HandleFunc("/locations", handlers.Locations)
 	http.HandleFunc("/events", handlers.Events)
+	http.HandleFunc("/test", DefaultPage)
+	
 	http.ListenAndServe(":8000", nil)
+	
+}
 
+func DefaultPage(w http.ResponseWriter, req *http.Request){
+	
+	var itemsPerPage int = 20
+	var firstID int = 1
+	
+	artists := GetArtists(firstID, itemsPerPage)
+	
+	pageArtists := PageDataArtist{Artists: artists}
+	
+	t := template.Must(template.ParseFiles("./template/home.html", "./template/layout/header.html", "./template/layout/footer.html"))
+	
+	t.Execute(w, pageArtists)
+	fmt.Print("TEST - ✅\n")
+	fmt.Print(pageArtists)
+
+	
+	
+}
+
+func GetArtists(first int, nbItems int) []Artist{
+	var artistList []Artist = []Artist{}
+	
+	i := first 
+	for i < first+nbItems {
+		res, err := http.Get(RESTAPIURL+"id/"+strconv.Itoa(i)+".json")
+		if err != nil{
+			fmt.Println(err)
+			return nil
+		}
+		
+		data, err := ioutil.ReadAll(res.Body)
+		
+		if err != nil{
+			fmt.Println(err)
+			return nil
+		}
+		
+		var tmpArt Artist
+		json.Unmarshal(data, &tmpArt)
+		// if tmpArt.ID != 0 {
+			fmt.Println(i)
+			artistList= append(artistList, tmpArt)
+			i++
+		// }	
+	}
+	return artistList
 }
--- static/scripts/data.js
+++ static/scripts/data.js
@@ -1,10 +1,13 @@
-
+const artistes = "https://cors-anywhere.herokuapp.com/https://groupietrackers.herokuapp.com/api/artists"
+const locations = "https://groupietrackers.herokuapp.com/api/locations"
+const dates = "https://groupietrackers.herokuapp.com/api/dates"
+const relations = "https://groupietrackers.herokuapp.com/api/relation"
 function dataBase(test){
-    console.log(test)
+    console.log(test[0].members[0])
+    console.log(test[0].members[1])
+    console.log(test[0].members[2])
 }
 
-
-fetch('https://groupietrackers.herokuapp.com')
+fetch(api)
 .then((response) => response.json())
 .then(dataBase)
-
