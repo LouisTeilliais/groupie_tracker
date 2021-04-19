63ea0583e868ddfe33654e005a591abc307591e3
root <root@DESKTOP-CI44KCS.localdomain>
2021-04-19 09:26:45 +0200

    Maps OK

--- static/scripts/maps.js
+++ static/scripts/maps.js
@@ -1,7 +1,7 @@
-function LocationsMaps(){
+function initMap(){
     // let gps = {lat : 47.205617056212475, lng : -1.539378717397206}
     let map = new google.maps.Map(document.getElementById("map"), {
-        zoom : 2,
+        zoom : 16,
         center : gps = {lat : 47.205617056212475, lng : -1.539378717397206},
         mapTypeID: "terrain",
     })
