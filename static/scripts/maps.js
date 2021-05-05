5c57706e4a83a1ca0e38ac46554f98ef02604bd5
Louis Teilliais <louis.teilliais@ynov.com>
2021-05-05 18:24:52 +0200

    header OK CSS Maps OK

--- static/scripts/maps.js
+++ static/scripts/maps.js
@@ -10,7 +10,7 @@ function initMap(){
         console.log("Longitude : " + lng)
         
         let map = new google.maps.Map(document.getElementById("map"), {
-            zoom : 10,
+            zoom : 7,
             center : gps = {lat, lng},
         });
         
@@ -39,18 +39,34 @@ function codeAddress(geocoder, resultsMap, marker) {
         let cities = await res.json()
         console.log(cities)
         
+        const divCities = document.getElementById("villes_artistes")
+        while (divCities.firstChild){
+            divCities.removeChild(divCities.firstChild);
+        }
+        // divCities.append("Les concerts de votre artiste :")
+        for (let i = 0; i < cities.length; i++){
+            
+            let replaceUnderscore = cities[i].replaceAll("_", " ")
+            let replace = replaceUnderscore.replace("-", " ")
+            const capitalize = replace.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+            let city = document.createElement("p");
+            city.textContent = capitalize
+            divCities.append(city)
+        }
+        console.log(divCities)
+        
         for (let i = 0; i < cities.length; i ++) {
             console.log(cities.length)
             
             geocoder.geocode({ address: cities[i]}, (results, status) => {
                 
                 if (status === "OK") {
-                      
+                    
                     // console.log(results[i])
                     resultsMap.setCenter(results[0].geometry.location);
                     
                     marker.setPosition(results[0].geometry.location)
-    
+                    
                 } else {
                     alert("Error: " + status);
                 }
